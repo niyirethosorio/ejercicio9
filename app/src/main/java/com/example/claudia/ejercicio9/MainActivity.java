4e81218154fa197c4f8915256156f5f665c63a01
niyirethosorio <niyirethosorio@gmail.com>
2016-06-30 02:25:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/claudia/ejercicio9/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.claudia.ejercicio9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1;
+    private int num;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.et1);
+        num=(int)(Math.random()*100001);
+        String cadena=String.valueOf(num);
+        Toast notificacion=Toast.makeText(this,cadena, Toast.LENGTH_LONG);
+        notificacion.show();
+    }
+
+    public void controlar(View v) {
+        String valorIngresado=et1.getText().toString();
+        int valor=Integer.parseInt(valorIngresado);
+        if (valor==num) {
+            Toast notificacion=Toast.makeText(this,"Muy bien recordaste el número mostrado.",Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+        else {
+            Toast notificacion=Toast.makeText(this,"Lo siento pero no es el número que mostré.",Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+    }
+}
